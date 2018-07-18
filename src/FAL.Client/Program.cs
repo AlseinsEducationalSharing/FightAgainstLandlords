60a0deef2272c936e76c6603d5560e029bc51dee
Alsein <xyh951115@live.com>
2018-07-18 21:17:03 +1000

    20180718

--- src/FAL.Client/Program.cs
+++ src/FAL.Client/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Alsein.Utilities;
+using Autofac;
+using FAL.Client.Services;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace FAL.Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args) => await ConfigureServices().Resolve<MainService>().Main(args);
+
+        private static IContainer ConfigureServices()
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ContainerBuilder();
+            var container = default(IContainer);
+            builder.Register(x => container).SingleInstance();
+            builder.RegisterType<HubConnectionBuilder>().SingleInstance();
+            builder.Register(x => x.Resolve<HubConnectionBuilder>().WithUrl("http://127.0.0.1:5000/fal").Build()).SingleInstance();
+            builder.RegisterAllServices();
+            container = builder.Build();
+            return container;
         }
     }
 }
--- src/FAL.Common/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace FAL.Common
-{
-    public class Class1
-    {
-    }
-}
